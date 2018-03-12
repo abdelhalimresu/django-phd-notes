b414a9d5dd74dd3f5b07e9d34b6dd15115444cde
abdelhalim@storepilots.com <abdelhalim@storepilots.com>
2018-03-12 23:31:13 +0000

    Customize forms

--- phdnotes/note/forms.py
+++ phdnotes/note/forms.py
@@ -3,10 +3,32 @@ from django import forms
 from .models import Note
 
 
-class NoteForm(forms.ModelForm):
+class BaseModelForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        kwargs.setdefault('label_suffix', '')
+        super(BaseModelForm, self).__init__(*args, **kwargs)
+
+
+class MyTextarea(forms.Textarea):
+    template_name = 'note/my_text_area.html'
+
+    def __init__(self, attrs=None):
+        default_attrs = {'cols': '60', 'rows': '15'}
+        if attrs:
+            default_attrs.update(attrs)
+
+        super().__init__(default_attrs)
+
+
+class NoteForm(BaseModelForm):
+    content = forms.CharField(widget=MyTextarea())
+
     class Meta:
         model = Note
         fields = [
             'content',
             'tags',
         ]
+        help_texts = {
+            'tags': 'Separate tags with ","',
+        }
