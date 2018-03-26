2c4908bd30748592004c889d7dcd1e485e401576
abdelhalim@storepilots.com <abdelhalim@storepilots.com>
2018-03-26 01:26:44 +0100

    Use django form for abstract

--- phdnotes/reference/forms.py
+++ phdnotes/reference/forms.py
@@ -1,27 +1,8 @@
-from django import forms
-
+from django.forms import ModelForm
 from .models import Reference
 
 
-class BaseModelForm(forms.ModelForm):
-    def __init__(self, *args, **kwargs):
-        kwargs.setdefault('label_suffix', '')
-        super(BaseModelForm, self).__init__(*args, **kwargs)
-
-
-class MyTextarea(forms.Textarea):
-    template_name = 'note/my_text_area.html'
-
-    def __init__(self, attrs=None):
-        default_attrs = {'cols': '60', 'rows': '15'}
-        if attrs:
-            default_attrs.update(attrs)
-
-        super().__init__(default_attrs)
-
-
-class ReferenceForm(BaseModelForm):
-    abstract = forms.CharField(strip=False, widget=MyTextarea())
+class ReferenceForm(ModelForm):
 
     class Meta:
         model = Reference
