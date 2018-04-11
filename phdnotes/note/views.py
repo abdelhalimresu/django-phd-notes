e8b31278641c02202b3a9d3ed819899c9bc166c2
abdelhalim@storepilots.com <abdelhalim@storepilots.com>
2018-04-11 04:58:42 +0100

    Update NoteDetailView

--- phdnotes/note/views.py
+++ phdnotes/note/views.py
@@ -3,6 +3,7 @@ from django.urls import reverse_lazy
 
 from .models import Note
 from .forms import NoteForm
+from .forms import CommentForm
 
 
 class NoteListView(ListView):
@@ -16,8 +17,14 @@ class NoteListView(ListView):
         return Note.objects.all()
 
 
-class NoteDetailView(DetailView):
+class NoteDetailView(DetailView, CreateView):
+    template_name = 'note/note_detail.html'
     model = Note
+    form_class = CommentForm
+
+    def get_success_url(self):
+        note_id = self.object.note.id
+        return reverse_lazy('notes:detail', kwargs={"pk": note_id})
 
 
 class NoteCreateView(CreateView):
